642cbd780db0fd141eefbfc5e1c918d2e76bb321
Jonathan Werner <jwerner@posteo.de>
2015-04-19 19:40:12 +0200

    another glog

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,6 +1,6 @@
 # The rest of my fun git aliases
 # alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+# alias glog='git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit'
 # alias gp='git push origin HEAD'
 # alias gd='git diff'
 # alias gc='git commit'
